6664e46aff7fb716e2bb09ced32b85b9e3d4ff27
Michael Hinshaw <michaelhinshaw@gmail.com>
2020-07-21 14:36:14 -0700

    add 2nd assignment

---
+++ JS/Basic_Javascript_2.js
@@ -0,0 +1,10 @@
+/*function change(newColor) {
+  var elem = document.getElementById(colorSelector);
+  elem.style.color=newColor;
+}*/
+
+function myFunction() {
+  var sentence = "Well then you better get to it";
+  sentence += " or you\'re not going to make it.";
+  document.getElementById("addString").innerHTML=sentence;
+}
