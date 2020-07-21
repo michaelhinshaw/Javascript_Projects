c91213566fd7d06c2a20c102247cd30e915594c1
Michael Hinshaw <michaelhinshaw@gmail.com>
2020-07-21 13:51:50 -0700

    assingment 1

---
+++ JS/Basic_Javascript_1.js
@@ -0,0 +1,15 @@
+
+document.write(7 * 4); // this code does multiplication
+
+let mySong=" Starfish and Coffee"; //assign variables
+let myArtist=" Prince";
+
+window.alert(mySong+myArtist); // displays the name of my favorite artist and one of his songs on the page load
+
+function myFunction() { // Function similar to the example assignment where it makes the font red after the user has clicked the button.
+  var str = "This text is red";
+  var result = str.fontcolor("red");
+  document.getElementById("Red_text").innerHTML = result;
+}
+
+
